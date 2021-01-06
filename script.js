5f6e57ec03ccbb02765b058e2aea82ef8decbd10
mdayyan011 <65223164+mdayyan011@users.noreply.github.com>
2021-01-07 00:45:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).keydown(function(e){ 
+  if(e.which === 123){ 
+
+     return false; 
+
+  } 
+
+}); 
+$(document).bind("contextmenu",function(e) {  
+	e.preventDefault(); 
+ 
+}); 
+ 
+
+ 
+
+  var waypoint11 = new Waypoint({
+    element: document.querySelector('.heyguys'),
+    handler: function() {
+        anime({
+            targets: '.heyguys',
+            translateX: [-1000, 0],
+            delay: anime.stagger(200),
+            duration: 800  
+            
+        });
+    },
+    offset: '100%'
+})
+ 
+
+
+var waypoint13 = new Waypoint({
+  element: document.querySelector('.heyguys1'),
+  handler: function() {
+      anime({
+          targets: '.heyguys1',
+          translateX: [-1000, 0],
+          delay: anime.stagger(200),
+          duration: 800  
+          
+      });
+  },
+  offset: '100%'
+})
+ 
